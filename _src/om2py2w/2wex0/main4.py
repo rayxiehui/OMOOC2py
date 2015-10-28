0645169ee2d104e9923adf0657601744a25d0bfd
rayxiehui <xiehuiray@gmail.com>
2015-10-28 22:46:13 +0800

    修改一次，简单功能可以用了

--- _src/om2py2w/2wex0/main4.py
+++ _src/om2py2w/2wex0/main4.py
@@ -1,23 +1,25 @@
 # -*- coding: UTF-8 -*-
 from Tkinter import *
+from sys import argv
+script,filename = argv
 
 #先写入txt
 def reg():
 	s1 = e1.get()
 	t1 = len(s1)
-	filename = open(txt.txt,'r+')
-	filename.write("\n")
-	filename.write(s1)
-	filename.close()
+	filename1 = open(filename,'w+')
+	filename1.write("\n")
+	filename1.write(s1)
+	filename1.close()
 	read()
 
 def read():
 	s1 = e1.get()
 	t1 = len(s1)
-	filename2 = open(txt.txt,'r+')
+	filename2 = open(filename,'r+')
 	text2 = filename2.read()
-    te.insert(1.0,'text2')
-    e1.delete(0,t1)
+	te.insert( END,text2)
+	e1.delete(0,t1)
 
 
 
@@ -31,7 +33,8 @@ e1.grid(row = 0,column=1,sticky = E)
 te = Text(root,width =50,height =10)
 te.grid(row = 2,column = 0,sticky = W)
 
-button = Button(root,text = "提交"，command = reg)
+button = Button(root,text = "提交",command = reg)
+button.grid(row = 1,column = 0,sticky =E)
 
 # text.insert(1.0,'22222')
 root.mainloop()
